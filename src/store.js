167569916d27c69fabe35222fad5e8ed69f17483
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-19 17:44:02 -0300

    setup redux devtools for debug

--- src/store.js
+++ src/store.js
@@ -1,13 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
 import reducer from "./reducers";
 import rootSaga from "./sagas";
 
+import { createStore, applyMiddleware, compose } from 'redux';
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 // mount it on the Store
-export default createStore(reducer, applyMiddleware(sagaMiddleware));
 
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(reducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(sagaMiddleware)
+  ));
+
+/* export default createStore(reducer, applyMiddleware(sagaMiddleware));
+ */
 // then run the saga
 sagaMiddleware.run(rootSaga);
