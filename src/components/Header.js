a7d3c2f318c6b63024f0db40e94338f756f6004c
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-19 13:42:59 -0300

    header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,17 +9,23 @@ export default class Header extends Component {
         <div className="header-jumbotron--brand">
           <img src={Logo} alt="Logo for
           the ybms app"/>
-          <h1 className="text-center text-light">
+          <h1 className="header-jumbotron--title text-center text-light">
             <strong>Your Beloves Music and Series</strong>
             </h1>
         </div>
         <div className="header-jumbotron--links">
         <ul className="text-light">
-            <li>
+            <li className="breadcrumb-item">
               <a href="#">Home</a>
             </li>
-            <li >
-              <a href="#">Fav</a>
+            <li className="breadcrumb-item">
+              <a href="#">Favorites</a>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="#">Movies/Series</a>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="#">About</a>
             </li>
         </ul>
         </div>
