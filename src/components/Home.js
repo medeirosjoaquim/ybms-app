c90f44b49d0d344f5d08a76aeab17baaea4b6913
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-20 14:26:07 -0300

    add new image formats

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-
+import {baseurl} from '../config/base-url'
 
 const Home = () => {
-  const basePosterUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+
+
   let poster_path = ''
   const movies = useSelector(state => state.movies);
   if (movies !== undefined & movies.results !== undefined) {
-    poster_path = basePosterUrl + movies.results[0].poster_path;
+    poster_path = baseurl.banner + movies.results[0].poster_path;
   }
   return (
     <div>
