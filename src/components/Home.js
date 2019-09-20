249bddba0d911959dea05f44894cd05f0a355f74
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-20 13:53:01 -0300

    get poster path

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,15 @@ import { useSelector } from 'react-redux'
 
 
 const Home = () => {
-
-  const movies = useSelector(state => state.movies)
-  console.log(movies.results !== undefined ? movies.results.length : 'no results');
+  const basePosterUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+  let poster_path = ''
+  const movies = useSelector(state => state.movies);
+  if (movies !== undefined & movies.results !== undefined) {
+    poster_path = basePosterUrl + movies.results[0].poster_path;
+  }
   return (
     <div>
-    home component works! {movies.description}
+      <img src={!poster_path === '' || poster_path} alt="" />
     </div>
   )
 }
