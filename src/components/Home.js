27ae2157a074d30e340a1195bc8305fbf8b6289c
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-20 16:42:16 -0300

    add release year to movie banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,17 +7,19 @@ import {baseurl} from '../config/base-url'
 
 const Home = () => {
 
-  let poster_path = ''
-  let poster_description = ''
+  let poster_path = '';
+  let poster_description = '';
+  let release_date = '';
   const {movies} = useSelector(state => state);
   if (movies !== undefined & movies.results !== undefined) {
     poster_path = baseurl.banner + movies.results[0].poster_path;
     poster_description = movies.results[0].title;
+    release_date = movies.results[0].release_date.slice(0,4);
   }
   return (
     (movies !== undefined & movies.results !== undefined) ?
     <div>
-      <Banner title={poster_description} imgSrc={poster_path} imgAlt={`Banner for the movie ${poster_description}`} />
+      <Banner title={`${poster_description} - ${release_date}` } imgSrc={poster_path} imgAlt={`Banner for the movie ${poster_description}`} />
       </div> :
       <div><h2>Loading...</h2></div>
   )
