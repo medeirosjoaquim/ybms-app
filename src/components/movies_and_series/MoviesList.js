c85400c5f1cc8516b8e55dd69ad20e38cc8eca20
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-21 20:19:15 -0300

    card style, movie info style

--- src/components/movies_and_series/MoviesList.js
+++ src/components/movies_and_series/MoviesList.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 import {baseurl} from '../../config/base-url'
+import { FaStar } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
+
 import './styles.scss'
 const getMovies =  () => {
   let {movies} = useSelector(state => state);
@@ -12,7 +15,12 @@ const renderMovies = (movie, i) =>
   (<div key={i} id="movie">
     <div className="movie--card">
       <img src={poster + movie.poster_path} alt={`Poster for the movie ${movie.title}`}></img>
-      <div className="movie--info">{movie.vote_average}</div>
+      <div className="movie--info">
+      <span className="movie-average"><FaHeart/></span>
+        <span className="movie-average"><FaStar />
+          <span className="movie-rating">{movie.vote_average}</span>
+        </span>
+      </div>
     </div>
 
   </div>);
--- src/components/movies_and_series/SeriesList.js
+++ src/components/movies_and_series/SeriesList.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {useSelector} from 'react-redux'
+import {baseurl} from '../../config/base-url'
+import './styles.scss'
 
-export default class SeriesList extends Component {
-  render() {
+const getSeries =  () => {
+  let {series} = useSelector(state => state);
+  return series.results;
+}
+const SeriesList = () => {
+  console.log(getSeries())
     return (
       <div>
         series list
       </div>
     )
-  }
 }
+
+export default SeriesList
