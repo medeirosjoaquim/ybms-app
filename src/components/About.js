57a707c51f9026a170c28ff90a4b6e003cd604b4
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-20 21:09:08 -0300

    more styles and about beginning

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,19 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about">
-      <div className="about--title"><h4>About us</h4></div>
-      <div className="about--content">
-        <p> There's something about us I want to say. Cause there's something between us anyway
-        </p>
-        <p>
-
-        </p>
+<div className="about--title font-weight-bolder">
+        <h3>About us</h3>
       </div>
+      <p>
+        To know a person's favorite movies is a thread that We can follow to understand what touches they.
+      </p>
+      {/* <div className="about--quotation">
+      <blockquote><p class="quotation">
+      No other art medium – neither painting nor poetry – can communicate the specific quality of the dream as film can</p>
+      <footer>— Ingmar Bergman</footer>
+      </blockquote>
+      </div> */}
+
     </div>
   );
 }
