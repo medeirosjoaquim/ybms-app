2a536db1b890d094f57f780150c5c60e6e331873
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-22 12:46:32 -0300

    component refactor and order by date

--- src/components/movies_and_series/MoviesList.js
+++ src/components/movies_and_series/MoviesList.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {useSelector} from 'react-redux'
 import {baseurl} from '../../config/base-url'
-
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { FaStar } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa';
+import {dynamicSort} from '../../utils'
 
 import './styles.scss'
-const getMovies =  () => {
-  let {movies} = useSelector(state => state);
-  return movies.results;
-}
+
 const {poster} = baseurl;
+let moviesArray = [];
+const getMovies = () => {
+  let {movies} = useSelector(state => state);
+  moviesArray = movies.results;
+  return moviesArray;
+}
 
 const renderMovies = (movie, i) =>
   (<div key={i} id="movie">
@@ -23,16 +28,39 @@ const renderMovies = (movie, i) =>
         </span>
       </div>
     </div>
-
   </div>);
 
-const MoviesList = () => {
 
-  return (
-       <div className="movie--list">
-        {getMovies().map(renderMovies)}
+
+class MoviesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  toggleSortDate = (event) => {
+    this.setState({movieList: this.props.movieList.sort(dynamicSort('release_date'))})
+  }
+
+  render() {
+    console.log(this.props.movieList)
+    console.log('state', this.state)
+    return (
+      <div className="movies">
+      <div className="sort-header">
+        <button type="button" className="btn btn-link" onClick={this.toggleSortDate}>Order by release year</button>
+        <button type="button" className="btn btn-link">Order by movie name </button>
       </div>
-  )
+      <div className="movie--list">
+        {this.props.movieList.map(renderMovies)}
+      </div>
+    </div>
+    )
+  }
+}
+function mapStateToProps(state) {
+  const { movies } = state
+  return { movieList: movies.results }
 }
 
-export default MoviesList
+export default connect(mapStateToProps)(MoviesList)
---
+++ src/utils/index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {string} property
+ * created by Ege Özcan
+ */
+export const dynamicSort = (property) => {
+  let sortOrder = 1;
+  if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+  }
+  return function (a,b) {
+      let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+  }
+}
