a105618c92a4cca54ce2a8e8ca90e8358b813972
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-21 22:40:13 -0300

    mock movies for component test

--- src/components/tests/Home.test.js
+++ src/components/tests/Home.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from '../Home';
+import { shallow } from 'enzyme';
+
+
+jest.mock('react-redux', () => ({
+useDispatch: ()=> { },
+useSelector: () => ({movies: {results: [{poster: ''}]}})
+}));
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
+  ReactDOM.render(shallow(<Home />, div));
   ReactDOM.unmountComponentAtNode(div);
 });
