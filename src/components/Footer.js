036bafa5ac1eeaf9587c7d2c16d692bf7ae8316a
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-23 13:43:09 -0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
 const Footer = () => (
   <section>
     <footer className="footer">
-        By Joaquim. For Philips with <span role="img" aria-label="love">❤️</span>{ year }
+        By <a href="https://linkedin.com/in/joaquim-medeiros" target="_blank" rel="noopener noreferrer">Joaquim</a>. For Philips with <span role="img" aria-label="love">❤️</span>{ year }
     </footer>
   </section>
 );
